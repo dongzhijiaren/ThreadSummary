8d656795ad6ae4099ece3d7ec83021d1fa508aea
dongzhijiaren <gogoxiaodong@126.com>
2017-08-07 10:50:25 +0800

    新建工程

---
+++ Thread-iOS/Thread-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Thread-iOS
+//
+//  Created by ZD on 2017/8/7.
+//  Copyright © 2017年 JIXING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Thread-iOS/Thread-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Thread-iOS
+//
+//  Created by ZD on 2017/8/7.
+//  Copyright © 2017年 JIXING. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
